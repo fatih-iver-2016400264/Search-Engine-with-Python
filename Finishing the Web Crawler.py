8b99e54a9081eb22d5302daa14f5c26322bf2028
Fatih İver <fatih.iver@boun.edu.tr>
2018-01-25 16:39:13 +0300

    Add files via upload

--- Finishing the Web Crawler.py
+++ Finishing the Web Crawler.py
@@ -10,3 +10,21 @@ def crawl_web(seed):
             union(tocrawl, get_all_links(content))
             crawled.append(pagelink)
     return index
+
+def add_to_index(index, keyword, url):
+    for entry in index:
+        if entry[0] == keyword:
+            entry[1].append(url)
+            return
+    index.append([keyword, [url]])
+
+def lookup(index, keyword):
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return []
+
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
